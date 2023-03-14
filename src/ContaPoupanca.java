16d96dfeda85f6d3d3cbea76b3110a79ddd955a8
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2023-03-14 08:47:08 -0300

    contas

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,15 @@
+public class ContaPoupanca {
+    String agencia;
+    String conta;
+    float saldo;
+    float salario;
+
+    public ContaPoupanca(String agencia, String conta, float saldo, float salario) {
+        this.agencia = agencia;
+        this.conta = conta;
+        this.saldo = saldo;
+        this.salario = salario;
+    }
+
+
+}
