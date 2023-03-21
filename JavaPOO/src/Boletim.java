2e4375e50bd4ba977756de33e99e2cc1b0a154ae
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2023-03-21 10:27:26 -0300

    javapoo

---
+++ JavaPOO/src/Boletim.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+public class Boletim {
+
+    double n1;
+    double n2;
+    double media;
+
+//    public Boletim(double n1, double n2){
+//        this.n1 = n1;
+//        this.n2 = n2;
+//    }
+
+    public Boletim (){
+        n1 = 0;
+        n2 = 0;
+    }
+
+    void getNotas(){
+        n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a primeira nota"));
+        n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite a segunda nota"));
+    }
+
+    void imprimeBoletim(){
+        getNotas();
+        calculaMedia();
+        JOptionPane.showMessageDialog(null, "Dados do boletim: "+
+                "\nNota 1: "+n1+
+                "\nNota 2: "+n2+
+                "\nMedia: "+media+
+                "\nConceito: "+ verificarConceito());
+    }
+
+    void calculaMedia(){
+        media=(n1+n2)/2;
+    }
+
+    String verificarConceito(){
+        String conceito = "";
+        if(media >= 8 && media <= 10){
+            conceito = "A";
+        }
+        else if(media >= 6){
+            conceito = "B";
+        }
+        else if(media >= 4){
+            conceito = "C";
+        }
+        else{
+            conceito = "D";
+        }
+        return conceito;
+    }
+
+}
---
+++ JavaPOO/src/SwitchCase.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um número de 1 a 4");
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número de 1 a 4"));
+
+
+        switch (n){
+            case 1:
+//                System.out.println("Você escolheu 1");
+                JOptionPane.showMessageDialog(null, "Você escolheu 1");
+                break;
+
+            case 2:
+//                System.out.println("Você escolehu 2");
+                JOptionPane.showMessageDialog(null, "Você escolheu 1");
+                break;
+
+            case 3:
+//                System.out.println("Você escolheu 3");
+                JOptionPane.showMessageDialog(null, "Você escolheu 1");
+                break;
+
+            case 4:
+//                System.out.println("Você escolheu 4");
+                JOptionPane.showMessageDialog(null, "Você escolheu 1");
+                break;
+
+            default:
+                System.out.println("Número inválido");
+        }
+    }
+}
