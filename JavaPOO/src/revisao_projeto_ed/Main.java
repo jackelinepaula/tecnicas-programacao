1eb3307227f9256faf7be5e01813d6e2b06e005f
jackelinepaula <jackeline.menees@fatec.sp.gov.br>
2023-05-02 10:36:53 -0300

    ARRAY

--- JavaPOO/src/revisao_projeto_ed/Main.java
+++ JavaPOO/src/revisao_projeto_ed/Main.java
@@ -1,5 +1,8 @@
 package revisao_projeto_ed;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main( String[] args){
@@ -31,6 +34,47 @@ public class Main {
         }catch (Exception erro){
             erro.printStackTrace();
         }
+
+
+        //------------- Array List -------------
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("Elemento A");
+        arrayList.add("Elemento C");
+
+        System.out.println(arrayList);
+
+        arrayList.add(1, "Elemento B");
+
+        System.out.println("\n" + arrayList);
+
+        System.out.println(arrayList.get(1));
+
+        System.out.printf("tamanho do vetor:"+ arrayList.size());
+
+        boolean existe = arrayList.contains("Elemento ");
+
+        if (existe) {
+            System.out.println("\nElemento encontrado no vetor");
+        } else {
+            System.out.println("\nElemento não existe no vetor");
+        }
+
+        int posicao= arrayList.indexOf("Elemento C");
+        if (posicao> -1){
+            System.out.println("Elemento encontrado na posição: "+ posicao);
+        }
+        else{
+            System.out.println("Elemento não existe no vetor");
+        }
+
+        System.out.println(arrayList);
+
+        arrayList.remove(0);
+        arrayList.remove("Elemento C");
+
+        System.out.println(arrayList);
     }
 
 }
